a4da05b77af988be5005d219a532b899307d8ad6
Acanthiza <nigel.willoughby@sa.gov.au>
2021-09-17 17:17:13 +0930

    `make_rf_diagnostics` returns many `yardstick` metrics, and optionally averages over folds

--- R/make_rf_diagnostics.R
+++ R/make_rf_diagnostics.R
@@ -10,7 +10,11 @@
 #' @param env_cols Character. Name of columns with environmental data.
 #' @param folds Numeric. How many folds to use in cross-validation?
 #' @param reps Numeric. How many repeats of cross-validation?
-#' @param ntrees Numeric. How many trees in the random forest?
+#' @param use_trees Numeric. How many trees in the random forest?
+#' @param use_mtry Numeric. `mtry` value used by random forest.
+#' @param set_min FALSE or numeric. If numeric, classes in `clust_col` with less
+#' than `set_min` cases will be filtered.
+#' @param summarise_folds Logical. Return mean values for folds or each fold.
 #'
 #' @return
 #' @export
@@ -22,32 +26,48 @@
                            , env_cols
                            , folds = 3
                            , reps = 2
-                           , ntrees = 999
+                           , use_trees = 499
+                           , use_mtry = floor(sqrt(sum(df_recipe$var_info$role == "predictor")))
+                           , set_min = FALSE
+                           , summarise_folds = folds > 1
                            ) {
+
+    env_df_use <- if(!isFALSE(set_min)) {
+
+      env_df %>%
+        dplyr::add_count(!!ensym(clust_col)) %>%
+        dplyr::filter(n > set_min) %>%
+        dplyr::select(-n) %>%
+        dplyr::mutate(!!clust_col := factor(!!ensym(clust_col)))
+
+    } else env_df
+
 
     df_recipe <- recipes::recipe(as.formula(paste0(clust_col
                                                    , " ~ ."
                                                    )
                                             )
-                                 , data = env_df
+                                 , data = env_df_use
                                  ) %>%
-      recipes::update_role(any_of(context), new_role = "context") %>%
-      recipes::prep()
+      recipes::update_role(any_of(context), new_role = "context")
+
 
     df_mod <- parsnip::rand_forest(mode = "classification"
-                                   , trees = ntrees
+                                   , trees = use_trees
+                                   , mtry = use_mtry
                                    ) %>%
       parsnip::set_engine("randomForest")
+
 
     df_workflow <- workflows::workflow() %>%
       workflows::add_model(df_mod) %>%
       workflows::add_recipe(df_recipe)
 
-    df_cv <- rsample::vfold_cv(env_df
-                               , v = folds
-                               , repeats = reps
-                               , strata = all_of(clust_col)
-                               )
+    df_cv <- env_df_use %>%
+      rsample::vfold_cv(v = folds
+                        , repeats = reps
+                        , strata = !!ensym(clust_col)
+                        )
 
     mets <- yardstick::metric_set(yardstick::accuracy
                                   , yardstick::kap
@@ -69,10 +89,22 @@
                           , metrics = mets
                           )
 
-    tune::collect_metrics(df_fit_rs) %>%
-      dplyr::select(.metric,mean) %>%
-      tidyr::pivot_wider(names_from = ".metric"
-                         , values_from = "mean"
-                         )
+    df_res <- tune::collect_metrics(df_fit_rs
+                                    , summarize = FALSE
+                                    )
+
+    if(summarise_folds) {
+
+      fold_col <- names(which.max(lapply(df_res, function(x) sum(grepl("fold|Fold",x)))))
+
+      df_res <- df_res %>%
+        dplyr::group_by(across(-c(!!ensym(fold_col),.estimate))) %>%
+        dplyr::summarise(.estimate = mean(.estimate)) %>%
+        dplyr::ungroup()
+
+    }
+
+    return(df_res)
 
   }
+
