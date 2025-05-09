5bc23d554eeb4491c0b7256e44ef0dda895d7d2e
Acanthiza <nigel.willoughby@sa.gov.au>
2025-05-09 12:08:21 +0930

    make_rf_diagnostics only returns a summary over all tunes

--- R/make_rf_diagnostics.R
+++ R/make_rf_diagnostics.R
@@ -16,15 +16,16 @@
 #' of the reference system of the input data) for block size(s) if using
 #' [blockCV::spatialBlock()]. If reps > 1, an equivalent number of range_m
 #' values are required to ensure the folds are different between repetitions.
-#' `repeated_spcv_block`.
+#' `repeated_spcv_block`. Only needed if using spatial cross validation.
 #' @param set_min FALSE or numeric. If numeric, classes in `clust_col` with less
 #' than `set_min` cases will be filtered.
 #' @param mlr3_cv_method Method to use with [mlr3::rsmp()] (as character, e.g.
 #' "repeated_cv" or "repeated_spcv_block".
 #' @param coords Character vector of length 2. Names of columns in `env_df` with
-#' x and y coordinates.
+#' x and y coordinates. Only needed if using spatial cross validation.
 #' @param crs_df Coordinate reference system for `coords`. Passed to the
-#' `crs` argument of [sf::st_as_sf()].
+#' `crs` argument of [sf::st_as_sf()]. Only needed if using spatial cross
+#' validation.
 #'
 #' @return
 #' @export
@@ -122,7 +123,6 @@ make_rf_diagnostics <- function(env_df
     ## learner-------
     learner <- mlr3::lrn("classif.ranger"
                          , sample.fraction = samp_prop
-                         , mtry = to_tune(1, 10)
                          , num.trees = trees
                          )
 
@@ -142,15 +142,18 @@ make_rf_diagnostics <- function(env_df
   }
 
   # results --------
-  res <- results$score() %>%
+  res <- results$score() |>
     dplyr::mutate(metrics = purrr::map(prediction_test
-                                       , ~envModel::get_conf_metrics(truth_vec = .$truth
-                                                                     , pred_vec = .$response
-                                                                     )
+                                       , \(x) envModel::get_conf_metrics(truth_vec = x$truth
+                                                                         , pred_vec = x$response
+                                                                         )
                                        )
-                  ) %>%
-    dplyr::select(iteration, classif.ce, metrics) %>%
-    tidyr::unnest(cols = c(metrics))
+                  ) |>
+    dplyr::select(iteration, classif.ce, metrics) |>
+    tidyr::unnest(cols = c(metrics)) |>
+    dplyr::summarise(tunes = dplyr::n()
+                     , dplyr::across(dplyr::where(is.numeric), mean)
+                     )
 
   res$seconds <- as.numeric(difftime(Sys.time(), start_time, units = "secs"))
 
